3599b5b4e4ba59575da2935e05dbfe4b09ce5b75
ashcoder007 <143689066+ashcoder007@users.noreply.github.com>
2023-08-31 12:54:48 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class B
+{
+    
+	public static void main(String[] args)
+	{
+	    Scanner reader = new Scanner(System.in);
+		System.out.println("enter an number:");
+		int number = reader.nextInt();
+		System.out.println("you entered integer:" +number);
+		
+	}
+}
