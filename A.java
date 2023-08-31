a614f94326856aaff8637ac57cf06cee94508537
ashcoder007 <143689066+ashcoder007@users.noreply.github.com>
2023-08-31 12:21:50 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+class calci {
+    private int i;
+    private int j;
+    calci(int x,int y)
+    {
+        i=x;
+        j=y;
+    }
+    public void add()
+    {
+        System.out.println(i+j);
+    }
+    public void sub()
+    {
+        System.out.println(i-j);
+    }
+    public void mul()
+    {
+        System.out.println(i*j);
+    }
+    public void div()
+    {
+     System.out.println(i/j);   
+    }
+}
+ public class A
+ {
+    public static void main(String[] args) 
+    {
+        calci c = new calci(3,4);
+        c.add();
+        c.sub();
+        c.mul();
+        c.div();
+    }
+}
