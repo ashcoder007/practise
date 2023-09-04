e6abc07d4646c511d6fdd296f7795ece5b08220d
ashcoder007 <143689066+ashcoder007@users.noreply.github.com>
2023-09-04 08:01:27 +0530

    Add files via upload

---
+++ E.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class E{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+
+        
+        if (number > 0) 
+        {
+            System.out.println(number + " is positive.");
+        }
+        else if (number < 0) 
+        {
+            System.out.println(number + " is negative.");
+        } 
+        else
+        {
+            System.out.println(number + " is zero.");
+        }
+
+       
+        scanner.close();
+    }
+}
