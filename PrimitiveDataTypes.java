e73b6f844fa220e4b1ce34e3df2d9c063d653c73
ashcoder007 <143689066+ashcoder007@users.noreply.github.com>
2023-09-04 07:57:23 +0530

    Add files via upload

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,35 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        
+        boolean isJavaFun = true;
+        System.out.println("Is Java fun? " + isJavaFun);
+
+      
+        char grade = 'A';
+        System.out.println("My grade is: " + grade);
+
+        
+        byte age = 25;
+        System.out.println("Age: " + age);
+
+        
+        short numberOfStudents = 500;
+        System.out.println("Number of students: " + numberOfStudents);
+
+        
+        int population = 1000000;
+        System.out.println("Population: " + population);
+
+        
+        long bigNumber = 12345678900L;
+        System.out.println("A really big number: " + bigNumber);
+
+       
+        float temperature = 98.6f;
+        System.out.println("Body temperature: " + temperature);
+
+      
+        double pi = 3.14159265359;
+        System.out.println("Value of Pi: " + pi);
+    }
+}
