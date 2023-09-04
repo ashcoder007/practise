472deebf8465c752c3adfcd98652a8d0dbc05fc9
ashcoder007 <143689066+ashcoder007@users.noreply.github.com>
2023-09-04 08:14:53 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+public class Sum
+{  
+public static void main(String[] args)   
+{  
+int i, num = 20, sum = 0;  
+
+for(i = 1; i <= num; ++i)  
+{  
+sum = sum + i;  
+}  
+
+System.out.println("Sum of Natural Numbers is = " + sum);  
+}  
+}  
